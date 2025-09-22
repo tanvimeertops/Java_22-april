c74ce027d3fa23f7efd7f0a39309f66579d82f7d
tanvi <tanvimeer.tops@gmail.com>
2025-09-22 11:11:58 +0530

    22 sep

--- src/com/exception/ExceptionDemo.java
+++ src/com/exception/ExceptionDemo.java
@@ -6,8 +6,35 @@ package com.exception;
  * runtime : Arithmetic exception ,number format exception
  * error: developer mistake 
  * 
+ * try : 
+ * 	1.if we have a doubt that any may have error use try with that code
+ * 	2. errors are thrown at catch block
+ * 	3. the line from which error is there, that line will be skipped 
+ * and rest of the lines will be executed
+ * catch :catch the error and trace back the error
+ * finally :whether error in there or not finally will executed
+ * throw 
+ * throws
  * 
  */
 public class ExceptionDemo {
-
+public static void main(String[] args) {
+	int a=10,b=0;
+	int[] x=new int[3];
+	try {
+		
+		System.out.println("div is :"+a/b);
+		for (int i = 1; i <=4; i++) {
+			x[i]=i;
+			System.out.println(x[i]);
+		}
+	} catch (Exception e) {
+	}
+	
+	finally {
+		System.out.println("Hi name is Tops");
+	}
+	
+	System.out.println("This code is written by tops on 22 sep");
+}
 }
---
+++ src/com/exception/UserDefineException.java
@@ -0,0 +1,69 @@
+package com.exception;
+
+import java.time.temporal.TemporalAmount;
+import java.util.Scanner;
+
+/*User defined exception :
+ * balance:1000
+ * deposit :2000
+ * balance: 3000
+ * withdraw: 5000
+ *	If you deposit 2000rs more you can withdraw 5000rs
+ * THROW :
+ * 1.written inside the method
+ * 2.throw followed by new keyword and constructor
+ * 3.must inherit exception class to make user defined exception
+ * throws: 
+ * written after method declaration
+ * can be a user defined or system defined exceptoin
+ */
+class TanviException extends Exception{
+	double amount;
+	public TanviException(double amount) {
+		
+		this.amount=amount;
+	}
+}
+class ATM{
+		double balance=1000;
+	public void deposit(double amount) {
+		balance+=amount;
+		System.out.println("you have deposited "+amount+
+				" your current balance is "+balance);
+	}
+	
+	public void withdraw(double amount) throws TanviException {
+		if(amount<balance) {
+			balance-=amount;
+			System.out.println("you have withdrawn "+amount+
+					" your current balance is "+balance);
+		}else {
+			double needs=amount-balance;
+				//      	5000-4000
+			throw new TanviException(needs);
+		}
+		
+	}
+}
+public class UserDefineException {
+public static void main(String[] args) {
+	ATM atm=new ATM();
+	Scanner sc=new Scanner(System.in);
+	double amount;
+	System.out.println("enter the amount to be deposited: ");
+	amount=sc.nextDouble(); //3000
+	atm.deposit(amount);
+	System.out.println("enter the amount to be withdrawn: ");
+	amount=sc.nextDouble();//5000
+	try {
+		atm.withdraw(amount);
+	} catch (TanviException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		System.out.println("If you deposit "+e.amount+" rs"+ 
+		"more you can withdraw "+amount);
+	}
+	
+	
+}
+}
